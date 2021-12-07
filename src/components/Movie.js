d0cba9e86b5d8066ea647d66e016951acad2b8d6
cc <votaryofturms@icloud.com>
2021-12-07 09:49:51 -0500

    passed movies props to Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,13 +5,13 @@ const Movie = (props) => {
     const { id } = useParams();
     const { push } = useHistory();
 
-    const movies = [];
-    const movie = movies.find(movie=>movie.id===Number(id));
-    
-    return(<div className="modal-page col">
+    const { movies } = props;
+    const movie = movies.find(movie => movie.id === Number(id));
+
+    return (<div className="modal-page col">
         <div className="modal-dialog">
             <div className="modal-content">
-                <div className="modal-header">						
+                <div className="modal-header">
                     <h4 className="modal-title">{movie.title} Details</h4>
                 </div>
                 <div className="modal-body">
@@ -35,10 +35,10 @@ const Movie = (props) => {
                                 <p><strong>{movie.description}</strong></p>
                             </div>
                         </section>
-                        
+
                         <section>
                             <span className="m-2 btn btn-dark">Favorite</span>
-                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete"/></span>
+                            <span className="delete"><input type="button" className="m-2 btn btn-danger" value="Delete" /></span>
                         </section>
                     </div>
                 </div>
