ba716e6fc8c1eb3139ba4f10f7ac1e75e441fffe
cc <votaryofturms@icloud.com>
2021-12-07 09:47:10 -0500

    replaced static movie variable in MovieList component with props

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -3,30 +3,30 @@ import React from 'react';
 import MovieListItem from './MovieListItem';
 import MovieFooter from './MovieFooter';
 
-const MovieList = (props)=> {
-    const movies = [];
+const MovieList = (props) => {
+    const { movies } = props;
 
     return (
         <div className="col">
             <table className="table table-striped table-hover">
                 <thead>
-                <tr>
-                    <th>Title</th>
-                    <th>Director</th>
-                    <th>Genre</th>
-                    <th>Metascore</th>
-                    <th></th>
-                </tr>
+                    <tr>
+                        <th>Title</th>
+                        <th>Director</th>
+                        <th>Genre</th>
+                        <th>Metascore</th>
+                        <th></th>
+                    </tr>
                 </thead>
 
                 <tbody>
                     {
-                        movies.map(movie=><MovieListItem key={movie.id} movie={movie}/>)
+                        movies.map(movie => <MovieListItem key={movie.id} movie={movie} />)
                     }
                 </tbody>
             </table>
-            
-            <MovieFooter totalMovies={movies.length}/>
+
+            <MovieFooter totalMovies={movies.length} />
         </div>
     );
 }
